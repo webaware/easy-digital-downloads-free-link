5a2f1061b794e9b19e3de752d3d4d38e73055abf
Javad Ahshamian <ham3da.j@gmail.com>
2021-03-10 12:33:59 +0330

    Added hook for free download url

--- templates/download-link.php
+++ templates/download-link.php
@@ -6,8 +6,9 @@
 if (!defined('ABSPATH')) {
 	exit;
 }
-?>
 
+$download_url = apply_filters('edd_free_download_url', $download_url);
+?>
 <a href="<?= esc_url($download_url); ?>" class="<?= esc_attr($download_link_classes); ?>">
 	<span class="edd-add-to-cart-label"><?= esc_html($download_label); ?></span>
 </a>
